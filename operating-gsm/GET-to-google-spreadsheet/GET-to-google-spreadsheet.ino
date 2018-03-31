c9b41f80e3ba905af21ae327571c6c89518dac92
deep <ddiwani3008@gmail.com>
2018-04-01 02:25:31 +0530

    debugging done, working as expected!

--- operating-gsm/GET-to-google-spreadsheet/GET-to-google-spreadsheet.ino
+++ operating-gsm/GET-to-google-spreadsheet/GET-to-google-spreadsheet.ino
@@ -55,53 +55,76 @@ String getString() {
     
     if (string == "z")
         return "z";
+        
     else if (string == "cpin")
         return "AT+CPIN?";
+        
     else if (string == "csq")
         return "AT+CSQ";
+        
     else if (string == "cipmode")
         return "AT+CIPMODE?";
+        
     else if (string == "cipmux")
         return "AT+CIPMUX?";
+        
     else if (string == "creg")
         return "AT+CREG?";
+        
     else if (string == "cgatt")
         return "AT+CGATT?";
+        
     else if (string == "cstt")
         return "AT+CSTT=\"airtelgprs.com\",\"\",\"\"";
+        
     else if (string == "ciicr")
         return "AT+CIICR";
+        
     else if (string == "cifsr")
         return "AT+CIFSR";
-    else if (string == "cipst")
-        return "AT+CIPSTART=\"TCP\",\"script.google.com\",\"80\"";
-    else if (string == "cipst2")
-        return "AT+CIPSTART=\"TCP\",\"api.thingspeak.com\",\"80\"";    
+        
     else if (string == "cipsend")
         return "AT+CIPSEND";
-    else if (string == "OKOK") {
-        lat = lat + 1.0;
-        lng = lng + 1.0;
-        return "PUT /macros/s/AKfycbysxw0m3rVjn70l7otnWSXp4MqB9kQm0zkInr1285hqHnPJprFI/exec/";
-    }
+    
+    else if (string == "sap31gprs")
+        return "AT+SAPBR=3,1,\"Contype\",\"GPRS\"";
+    
+    else if (string == "sap31apn")
+        return "AT+SAPBR=3,1,\"APN\",\"airtelgprs.com\"";
+    
+    else if (string == "sap11")
+        return "AT+SAPBR=1,1";
+        
+    else if (string == "sap21")
+        return "AT+SAPBR=2,1";
+        
     else if (string == "init")
         return "AT+HTTPINIT";
+        
     else if (string == "cid")
         return "AT+HTTPPARA=\"CID\",1";
-    else if (string == "url")
-        return "AT+HTTPPARA=\"URL\",\"http://script.google.com/macros/s/\"";
-    else if (string == "break1")
-        return "AT+HTTPPARA=\"BREAK\",\"AKfycbysxw0m3rVjn70l7otnWSXp\"";
-    else if (string == "break2")
-        return "AT+HTTPPARA=\"BREAK\",\"4MqB9kQm0zkInr1285hqHnPJprFI/exec\"";
-    else if (string == "data")
-        return "AT+HTTPDATA=15,15000";
-    else if (string == "insert")
-        return "lat=17&lng=25.6";
+        
+    else if (string == "url") {
+        lat = lat + 1.0;
+        lng = lng + 1.0;
+        return "AT+HTTPPARA=\"URL\",\"<url you want to request>\"";
+    }        
+ 
+    else if (string == "term")
+        return "AT+HTTPTERM";
+    
+    else if (string == "action")
+        return "AT+HTTPACTION=1";
+
+    else if (string == "read")
+        return "AT+HTTPREAD";
+        
     else
         return string;
 }
+
 //lat=" + String(lat) + "&lng=" + String(lng)
+
 void enterCTRLZ() {
     gps.println((char)26);
 }
